8a3de10bc2e65475f6f0b75a38a8009bf6b14689
Maksim Kirilko <maksimkirilko@gmail.com>
2020-02-08 22:21:57 +0300

    init: add configs

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+
+export class App extends React.PureComponent {
+    render() {
+        return (
+            <div className="app-container">
+
+            </div>
+        );
+    }
+}
